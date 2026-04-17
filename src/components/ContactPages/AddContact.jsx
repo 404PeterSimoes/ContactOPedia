c1ac3224180492ef84ed5a08bd2ea692e4de2fef
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-04-17 11:28:04 +0100

    Section 5 - 86 & 88 - Add Contact Form UI & Handle Form Submit

---
+++ src/components/ContactPages/AddContact.jsx
@@ -0,0 +1,56 @@
+function AddContact() {
+  function handleAddContactForm(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <div className="border col-12 text-white p-2">
+      <form onSubmit={handleAddContactForm}>
+        <div className="row p-2">
+          <div className="col-12 text-white-50 text-center h5">
+            Add a new Contact
+          </div>
+          <div className="col-12 col-md-4 p-1">
+            <input
+              placeholder="Name..."
+              name="name"
+              className="form-control form-control-sm"
+            />
+          </div>
+          <div className="col-12 col-md-4 p-1">
+            <input
+              placeholder="Email..."
+              name="email"
+              className="form-control form-control-sm"
+            />
+          </div>
+          <div className="col-12 col-md-4 p-1">
+            <input
+              placeholder="Phone..."
+              name="phone"
+              className="form-control form-control-sm"
+            />
+          </div>
+          <div className="col-12 text-center text-success">Success Message</div>
+          <div className="col-12 text-center text-danger">Error Message</div>
+          <div className="col-12">
+            <button
+              className="btn btn-primary btn-sm form-control"
+              type="submit"
+            >
+              Create
+            </button>
+          </div>
+          {/* <div className="col-12">
+            <button className="btn btn-danger btn-sm form-control">
+              Cancel
+            </button>
+          </div> */}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddContact;
